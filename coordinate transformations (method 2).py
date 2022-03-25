6b0c665dc8f251ffcd3bd861eef50e8b827d8d3a
Mahdi <97524950+Mahdi-abd-rahmen-Madi@users.noreply.github.com>
2022-03-25 17:03:19 +0100

    Add files via upload

---
+++ coordinate transformations (method 2).py
@@ -0,0 +1,10 @@
+import math 
+import time 
+import numpy as np
+from numpy import exp, abs, angle
+
+def polar2z(r,theta):
+    return r * exp( 1j * theta )
+
+def z2polar(z):
+    return ( abs(z), angle(z) )
