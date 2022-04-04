c667c13a7065593e368f79aba3ef46283f8ceafc
Mahdi <97524950+Mahdi-abd-rahmen-Madi@users.noreply.github.com>
2022-04-04 17:57:58 +0100

    Update coordinate transformations.py

--- coordinate transformations.py
+++ coordinate transformations.py
@@ -1,25 +1,22 @@
 import math
-
+# Function rect allow transformation from polar equation as input to float x,y 
 def rect(r, theta):
-    """theta in degrees
-    returns tuple; (float, float); (x,y)
-    """
-    # r ≥ 0 , 0° ≤ θ ≤ 180° (π rad) ,  0° ≤ φ < 360° (2π rad).
+    """ theta in degrees returns tuple; (float, float); (x,y) """
+    """ r ≥ 0 , 0° ≤ θ ≤ 180° (π rad) ,  0° ≤ φ < 360° (2π rad) """
     x = r * math.cos(math.radians(theta))
     y = r * math.sin(math.radians(theta))
     return x,y
-
+# Function polar allows user input of x and y and transform it into a polar equation   
 def polar(x, y):
-    """returns r, theta(degrees)
-    """
+    """ returns r, theta(degrees) """
     r = (x ** 2 + y ** 2) ** .5
     theta = math.degrees(math.atan2(y,x))
     return r, theta
 
+# class point allows to create a point from coordinates ( x , y ) 
 class Point(object):
     def __init__(self, x=None, y=None, r=None, theta=None):
-        """x and y or r and theta(degrees)
-        """
+        """x and y or r and theta(degrees)"""
         if x and y:
             self.c_polar(x, y)
         elif r and theta:
@@ -32,8 +29,7 @@ class Point(object):
         self._r, self._theta = f(self._x, self._y)
         self._theta_radians = math.radians(self._theta)
     def c_rect(self, r, theta, f = rect):
-        """theta in degrees
-        """
+        """ theta in degrees """
         self._r = r
         self._theta = theta
         self._theta_radians = math.radians(theta)
@@ -59,15 +55,13 @@ class Point(object):
         return self._r
     r = property(fget = getr, fset = setr)
     def settheta(self, theta):
-        """theta in degrees
-        """
+        """ theta in degrees """
         self.c_rect(self._r, theta)
     def gettheta(self):
         return self._theta
     theta = property(fget = gettheta, fset = settheta)
     def set_r_theta(self, r, theta):
-        """theta in degrees
-        """
+        """ theta in degrees """
         self.c_rect(r, theta)
     def get_r_theta(self):
         return self._r, self._theta
